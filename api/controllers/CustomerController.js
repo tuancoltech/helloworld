908d0c45b4a80b3c45262e4d335d1480ac87cd9a
Tuan <tuancoltech@gmail.com>
2016-12-12 14:34:54 +0700

    - Add Customer and display

--- api/controllers/CustomerController.js
+++ api/controllers/CustomerController.js
@@ -9,6 +9,28 @@ module.exports = {
 
     'new': function(req, res) {
         res.view();
+    },
+
+    create: function(req, res, next) {
+        Customer.create(req.params.all(), function customerCreated(err, customer) {
+            if (err) return next(err);
+
+//            res.json(customer);
+
+            res.redirect('/customer/show/' + customer.id);
+
+        });
+    },
+
+    show: function(req, res, next) {
+        Customer.findOne(req.param('id'), function foundCustomer(err, customer) {
+            if (err) return next(err);
+            if (!customer) return next();
+
+            res.view( {
+                customer: customer
+            });
+        });
     }
 
 };
